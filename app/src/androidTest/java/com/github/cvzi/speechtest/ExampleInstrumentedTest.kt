1679250acf55446ea26e3e724267607b6e9d2163
cvzi <cuzi@openmail.cc>
2023-01-01 16:30:31 +0100

    Create instrument test

--- app/src/androidTest/java/com/github/cvzi/speechtest/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/cvzi/speechtest/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.github.cvzi.speechtest
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.Manifest
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.GrantPermissionRule
+import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,23 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.RECORD_AUDIO)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.github.cvzi.speechtest", appContext.packageName)
     }
+
+    @Test
+    fun initiateVoiceInput() {
+        ActivityScenario.launch(MainActivity::class.java).use {
+            onView(withId(R.id.button)).perform(click())
+            Thread.sleep(1000)
+        }
+    }
+
+
 }
